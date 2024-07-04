71980fa2b3b0a240b2cf66524f117aeeb548de78
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2024-07-04 19:34:58 +0530

    some changes

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -8,25 +8,6 @@ router.get('/', function (req, res, _next) {
   res.status(200).json({ message: 'Working good' });
 });
 
-// const validateUsername = (req, res, next) => {
-//   if (!req.body.username) {
-//     req.validationErrors = {
-//       username: 'Username cannot be null',
-//     };
-//   }
-//   next();
-// };
-
-// const validateEmail = (req, res, next) => {
-//   if (!req.body.email) {
-//     req.validationErrors = {
-//       ...req.validationErrors,
-//       email: 'E-mail cannot be null',
-//     };
-//   }
-//   next();
-// };
-
 router.post(
   '/',
   UserServices.validateUsername('username'),
@@ -35,14 +16,14 @@ router.post(
   signup,
 );
 
-router.post('/token/:token', async (req, res, _next) => {
-  const token = req.params.token;
-  try {
-    await UserServices.activate(token);
-  } catch (err) {
-    return res.status(400).json({ message: req.t(err.message) });
-  }
-  res.json({ message: req.t('account_activation_success') });
-});
+// router.post('/token/:token', async (req, res, _next) => {
+//   const token = req.params.token;
+//   try {
+//     await UserServices.activate(token);
+//   } catch (err) {
+//     return res.status(400).json({ message: req.t(err.message) });
+//   }
+//   res.json({ message: req.t('account_activation_success') });
+// });
 
 module.exports = router;
